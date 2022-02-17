a8ceea1fff7285e86207ec334ed6270c91cdd650
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2022-02-17 20:47:15 +0300

    практика для себя

---
+++ practice/maxNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace maxNumber
+{
+    internal class Program
+    {
+        const string inputFileName = "input.txt";
+        const string outputFileName = "output.txt";
+        static void Main( string[] args )
+        {
+            string[] input = File.ReadAllLines( inputFileName );
+            string number = input[ 1 ];
+            var it = number.Min();
+            File.WriteAllText( outputFileName, ( number.IndexOf( it ) + 1 ).ToString() );
+        }
+    }
+}

451104e4dab44abeef90131c0cdcd793aa73fd66
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2022-02-17 16:23:33 +0300

    2.4 есть ошибки и медленно

--- 2.4/Program.cs
+++ 2.4/Program.cs
@@ -10,125 +10,65 @@ namespace _2._4
     {
         const string inputFileName = "input.txt";
         const string outputFileName = "output.txt";
+        const int INF = int.MaxValue / 2;
         static void Main( string[] args )
         {
             string[] input = File.ReadAllLines( inputFileName );
             int n = int.Parse( input[ 0 ] );
             List<int> fiel = input[ 1 ].Split( " " ).Select( ch => int.Parse( ch ) ).ToList();
+            int Max = fiel.Max();
+            List<int> nearest = Enumerable.Repeat( 0, n ).ToList();
+            List<int> pos = Enumerable.Repeat( -1, Max + 1 ).ToList();
 
-            List<int> next = Enumerable.Repeat( 0, n + 1 ).ToList();
-
-            List<int> last = Enumerable.Repeat( int.MaxValue / 2, n + 1 ).ToList();
-            for ( int i = n - 1; i > 0; i-- )
+            for ( int i = n - 1; i >= 0; i-- )
             {
-                next[ i ] = last[ fiel[ i ] ];
-                last[ fiel[ i ] ] = i;
+                nearest[ i ] = pos[ fiel[ i ] ];
+                pos[ fiel[ i ] ] = i;
             }
-
-            List<int> minJumps = Enumerable.Repeat( int.MaxValue / 2, n + 1 ).ToList();
-            List<int> jumpTo = Enumerable.Repeat( 0, n + 1 ).ToList();
-            List<Pair> tree = Enumerable.Repeat( new Pair { minJump = int.MaxValue / 2, where = 0 }, 2 + 2 * n ).ToList();
-            minJumps[ n ] = 0;
-            Update( tree, n, new Pair { minJump = 0, where = n } );
-            for ( int i = n - 1; i >= 1; i-- )
+            List<int> dist = Enumerable.Repeat( INF, n ).ToList();
+            List<int> from = Enumerable.Repeat( -1, n ).ToList();
+            List<Pair> queue = new List<Pair>();
+            queue.Add( new Pair() );
+            for ( int i = 0; i < n; i++ )
             {
-                if ( next[ i ] == 0 )
+                queue = queue.OrderBy( p => p.value ).ToList();
+                queue.Reverse();
+                while ( queue.Count > 0 && queue[ 0 ].time < i )
                 {
-                    Update( tree, i, new Pair { minJump = int.MaxValue / 2, where = i } );
+                    queue.Remove( queue[ 0 ] );
                 }
-                else
+                if ( queue.Count == 0 )
                 {
-                    Pair pair = GetMin( tree, i + 1, next[ i ] );
-                    minJumps[ i ] = pair.minJump;
-                    jumpTo[ i ] = pair.where;
+                    Console.WriteLine( "0" );
+                    return;
                 }
-                Update( tree, i, new Pair { minJump = minJumps[ i ], where = i } );
-            }
-            if ( minJumps[ 1 ] == int.MaxValue / 2 )
-            {
-                Console.WriteLine( "0" );
-            }
-            else
-            {
-                Console.WriteLine( minJumps[ 1 ] );
-                for ( int i = 1; i < n; i = jumpTo[ i ] )
+                dist[ i ] = queue[ 0 ].value;
+                from[ i ] = queue[ 0 ].from;
+                queue.Add( new Pair()
                 {
-                    if ( i > 1 )
-                    {
-                        Console.Write( " " );
-                    }
-                    Console.WriteLine( i );
-                }
+                    value = dist[ i ] + 1,
+                    time = nearest[ i ],
+                    from = i
+                } );
             }
+            List<int> result = new List<int>();
+            for ( int i = dist[ n - 1 ], j = from[ n - 1 ]; i > 0; i-- )
+            {
+                result.Add( j + 1 );
+                j = from[ j ];
 
+            }
+            result.Reverse();
+
+            Console.WriteLine( string.Join( " ", result.Select( ch => ch.ToString() ) ) );
             Console.WriteLine( "Hello World!" );
         }
 
-        bool Checker( Pair left, Pair right )
+        public struct Pair
         {
-            return left.minJump < right.minJump;
+            public int value;
+            public int time;
+            public int from;
         }
-
-        static void Update( List<Pair> tree, int index, Pair value )
-        {
-            index += tree.Count / 2;
-            tree[ index ] = value;
-
-            // tree[index] = min(tree[index * 2], tree[index * 2 + 1];
-
-            while ( index > 1 )
-            {
-                index >>= 1;
-                tree[ index ] = Min( tree[ index * 2 ], tree[ index * 2 + 1 ] );
-            }
-        }
-
-        static Pair GetMin( List<Pair> tree, int left, int right )
-        {
-            left += tree.Count >> 1;
-            right += tree.Count >> 1;
-            Pair result = new Pair() { minJump = int.MaxValue / 2, where = 0 };
-
-            while ( left <= right )
-            {
-                if ( left % 2 == 0 )
-                {
-                    left /= 2;
-                }
-                else
-                {
-                    result = Min( result, tree[ left ] );
-                    left++;
-                    left /= 2;
-                }
-                if ( right % 2 == 1 )
-                {
-                    right /= 2;
-                }
-                else
-                {
-                    result = Min( result, tree[ left ] );
-                    right--;
-                    right /= 2;
-                }
-            }
-
-            return result;
-        }
-
-        static Pair Min( Pair left, Pair right )
-        {
-            if ( left.minJump < right.minJump )
-            {
-                return left;
-            }
-            return right;
-        }
-    }
-
-    struct Pair
-    {
-        public int minJump;
-        public int where;
     }
 }

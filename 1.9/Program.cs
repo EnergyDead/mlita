2b9db95158d7123e47fc3028e5a24f607aa099b3
Ruslan Vasilev <ruslan.vasilev@travelline.ru>
2022-02-21 13:17:12 +0300

    1.9 работает (надо оптимизировать)

--- 1.9/Program.cs
+++ 1.9/Program.cs
@@ -12,88 +12,124 @@ namespace _1._9
         static void Main( string[] args )
         {
             List<Lake> lakes = ReadLakes();
-            List<int> result = new List<int>();
+            List<int> result = new List<int>( lakes.Count );
 
             foreach ( var lake in lakes )
             {
                 result.Add( SearchDam( lake ) );
             }
-            Console.WriteLine( String.Join(" ", result ) );
+            File.WriteAllText( outputFileName, String.Join( " ", result ) );
         }
 
         private static int SearchDam( Lake lake )
         {
-            List<int> result = new List<int>();
-            for ( int x = 0; x < lake.width; x++ )
+            if ( lake.points.Length == 2 )
             {
-                for ( int y = 0; y < lake.height; y++ )
+                return 1;
+            }
+            for ( int y = 0; y < lake.height; y++ )
+            {
+                for ( int x = 0; x < lake.width; x++ )
                 {
-                    Point point = new Point( x, y, (char)lake.points[ y, x ] );
-                    if ( point.type == Type.water )
+                    if ( lake.points[ y, x ] == Type.water )
                     {
-                        if ( lake.points[ y, x - 1 ] == Type.water && lake.points[ y, x + 1 ] == Type.water && lake.points[ y - 1, x ] == Type.land && lake.points[ y + 1, x ] == Type.land )
+                        if ( x + 1 < lake.width && lake.points[ y, x + 1 ] == Type.water )
                         {
-                            result.Add( 1 );
-                        }
-
-                        if ( lake.points[ y, x - 1 ] == Type.water && lake.points[ y, x + 1 ] == Type.water )
-                        {
-                            if ( lake.points[ y - 1, x ] == Type.land && lake.points[ y + 1, x ] == Type.water )
+                            if ( ( ( y + 1 ) >= lake.height ) || ( lake.points[ y + 1, x ] == Type.land || lake.points[ y + 1, x + 1 ] == Type.land ) )
                             {
-                                int count = 0;
-                                int Ynext = y;
-                                while ( lake.points[ Ynext, x ] == Type.water )
+                                if ( ( ( y - 1 ) < 0 ) || ( lake.points[ y - 1, x ] == Type.land || lake.points[ y - 1, x + 1 ] == Type.land ) )
                                 {
-                                    count++;
-                                    Ynext++;
-                                }
-                                result.Add( count );
-                            }
+                                    Lake points = (Lake)lake.Clone();
 
-                            if ( lake.points[ y + 1, x ] == Type.land && lake.points[ y - 1, x ] == Type.water )
-                            {
-                                int count = 0;
-                                int Ynext = y;
-                                while ( lake.points[ Ynext, x ] == Type.water )
-                                {
-                                    count++;
-                                    Ynext--;
+                                    if ( CountLend2( points, y, x + 1 ) )
+                                    {
+                                        return 1;
+                                    }
+
+                                    points.points[ y, x + 1 ] = Type.land;
+
+                                    if ( HasMoreLand( points, y, x ) > 1 )
+                                    {
+                                        return 1;
+                                    }
                                 }
-                                result.Add( count );
                             }
                         }
-
-                        if ( lake.points[ y - 1, x ] == Type.water && lake.points[ y + 1, x ] == Type.water )
+                        if ( y + 1 < lake.height && lake.points[ y + 1, x ] == Type.water )
                         {
-                            if ( lake.points[ y, x - 1 ] == Type.land && lake.points[ y, x + 1 ] == Type.water )
+                            if ( ( ( x + 1 ) >= lake.width ) || ( lake.points[ y, x + 1 ] == Type.land || lake.points[ y + 1, x + 1 ] == Type.land ) )
                             {
-                                int count = 0;
-                                int Xnext = x;
-                                while ( lake.points[ y, Xnext ] == Type.water )
+                                if ( ( ( x - 1 ) < 0 ) || ( lake.points[ y, x - 1 ] == Type.land || lake.points[ y + 1, x - 1 ] == Type.land ) )
                                 {
-                                    count++;
-                                    Xnext++;
-                                }
-                                result.Add( count );
-                            }
+                                    Lake points = (Lake)lake.Clone();
 
-                            if ( lake.points[ y + 1, x ] == Type.land && lake.points[ y - 1, x ] == Type.water )
-                            {
-                                int count = 0;
-                                int Xnext = x;
-                                while ( lake.points[ y, Xnext ] == Type.water )
-                                {
-                                    count++;
-                                    Xnext--;
+                                    if ( CountLend( points, y + 1, x ) )
+                                    {
+                                        return 1;
+                                    }
+                                    points.points[ y + 1, x ] = Type.land;
+                                    if ( HasMoreLand( points, y, x ) > 1 )
+                                    {
+                                        return 1;
+                                    }
                                 }
-                                result.Add( count );
                             }
                         }
                     }
                 }
             }
 
-            return result.Min();
+            return 2;
+        }
+
+        private static bool CountLend( Lake points, int y, int x )
+        {
+            var a = ( y + 1 < points.points.GetLength( 0 ) && points.points[ y + 1, x ] == Type.land );
+            var b = ( x + 1 < points.points.GetLength( 1 ) && points.points[ y, x + 1 ] == Type.land );
+            var c = ( x - 1 > 0 && points.points[ y, x - 1 ] == Type.land );
+            return a && b && c;
+        }
+
+        private static bool CountLend2( Lake points, int y, int x )
+        {
+            var a = ( x + 1 < points.points.GetLength( 1 ) && points.points[ y, x + 1 ] == Type.land );
+            var b = ( y + 1 < points.points.GetLength( 0 ) && points.points[ y + 1, x ] == Type.land );
+            var c = ( y - 1 > 0 && points.points[ y - 1, x ] == Type.land );
+            return a && b && c;
+        }
+
+        private static int HasMoreLand( Lake points, int y, int x )
+        {
+            var f = points.points;
+            int count = 0;
+            for ( int i = y; i < f.GetLength( 0 ); ++i )
+            {
+                for ( int j = x; j < f.GetLength( 1 ); ++j )
+                {
+                    if ( f[ i, j ] == Type.water )
+                    {
+                        dfs( f, i, j );
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void dfs( Type[,] f, int i, int j )
+        {
+            if ( f[ i, j ] == Type.water )
+            {
+                f[ i, j ] = Type.land;
+                if ( i + 1 < f.GetLength( 0 ) )
+                    dfs( f, i + 1, j );
+                if ( i - 1 >= 0 )
+                    dfs( f, i - 1, j );
+                if ( j + 1 < f.GetLength( 1 ) )
+                    dfs( f, i, j + 1 );
+                if ( j - 1 >= 0 )
+                    dfs( f, i, j - 1 );
+            }
         }
 
         private static List<Lake> ReadLakes()
@@ -109,13 +145,22 @@ namespace _1._9
             for ( int i = 0; i < lakesCount; i++ )
             {
                 var size = input.Dequeue().Split( " " ).Select( s => int.Parse( s ) );
-                Lake lake = new Lake( size.First(), size.Last() );
+                Lake lake = new Lake( size.First() + 2, size.Last() + 2 );
+
                 for ( int y = 0; y < lake.height; y++ )
                 {
-                    var line = input.Dequeue();
-                    for ( int x = 0; x < line.Length; x++ )
+                    for ( int x = 0; x < lake.width; x++ )
                     {
-                        lake.points[ y, x ] = (Type)line[ x ];
+                        lake.points[ y, x ] = Type.land;
+                    }
+                }
+
+                for ( int y = 0; y < size.First(); y++ )
+                {
+                    var line = input.Dequeue();
+                    for ( int x = 0; x < size.Last(); x++ )
+                    {
+                        lake.points[ y + 1, x + 1 ] = (Type)line[ x ];
                     }
                 }
                 lakes.Add( lake );
@@ -124,7 +169,9 @@ namespace _1._9
             return lakes;
         }
     }
-    struct Lake
+
+
+    struct Lake : ICloneable
     {
         public Lake( int h, int w )
         {
@@ -132,30 +179,19 @@ namespace _1._9
             height = h;
             points = new Type[ h, w ];
         }
-
-        /// <summary>
-        /// ширина
-        /// </summary>
         public int width;
-
-        /// <summary>
-        /// высота
-        /// </summary>
         public int height;
-        // public List<Point> points;
         public Type[,] points;
-    }
-    struct Point
-    {
-        public Point( int x, int y, char type )
+
+        public object Clone()
         {
-            this.x = x;
-            this.y = y;
-            this.type = (Type)type;
+            var lake = new Lake( height, width );
+            lake.points = (Type[,])points.Clone();
+
+            return lake;
         }
-        public int x, y;
-        public Type type;
     }
+
     enum Type
     {
         water = '#',

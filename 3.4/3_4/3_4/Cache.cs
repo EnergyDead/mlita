87bcf6ede5464098f80e2ee7d7f65666d965745f
Ruslan <rusvas012@gmail.com>
2022-04-03 13:23:16 +0300

    Вынес класс

---
+++ 3.4/3_4/3_4/Cache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_4
+{
+    public class Cache
+    {
+        private readonly ulong?[] _items;
+        private int _maxX;
+        private int _maxY;
+
+        public Cache(int mX, int mY, int mZ)
+        {
+            _maxX = mX;
+            _maxY = mY;
+            _items = new ulong?[mX * mY * mZ];
+        }
+
+        public ulong? Get(int x, int y, int z)
+        {
+            var pos = GetPos(x, y, z);
+
+            return _items[pos];
+        }
+
+        public void Set(int x, int y, int z, ulong? value)
+        {
+            var pos = GetPos(x, y, z);
+
+            _items[pos] = value;
+        }
+
+        private int GetPos(int x, int y, int z)
+        {
+            return z * _maxY * _maxX + y * _maxX + x;
+        }
+    }
+}
--- 3.4/3_4/3_4/Program.cs
+++ 3.4/3_4/3_4/Program.cs
@@ -119,38 +119,5 @@ namespace _3_4
 
             return ((2 * lastDigit + (progressiveCount - 1)) * progressiveCount) / 2 + (digitsCount - progressiveCount) * 9;
         }
-
-        private class Cache
-        {
-            private readonly ulong?[] _items;
-            private int _maxX;
-            private int _maxY;
-
-            public Cache(int mX, int mY, int mZ)
-            {
-                _maxX = mX;
-                _maxY = mY;
-                _items = new ulong?[mX * mY * mZ];
-            }
-
-            public ulong? Get(int x, int y, int z)
-            {
-                var pos = GetPos(x, y, z);
-
-                return _items[pos];
-            }
-
-            public void Set(int x, int y, int z, ulong? value)
-            {
-                var pos = GetPos(x, y, z);
-
-                _items[pos] = value;
-            }
-
-            private int GetPos(int x, int y, int z)
-            {
-                return z * _maxY * _maxX + y * _maxX + x;
-            }
-        }
     }
 }

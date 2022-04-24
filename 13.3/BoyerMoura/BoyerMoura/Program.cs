6bf66f4c868ec237b68f07ea7a142244acc633b5
Ruslan <rusvas012@gmail.com>
2022-04-24 23:06:39 +0300

    13.3 Боера Мура - поиск подстроки в строке

---
+++ 13.3/BoyerMoura/BoyerMoura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoyerMoura
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( DateTime.Now );
+        }
+    }
+}

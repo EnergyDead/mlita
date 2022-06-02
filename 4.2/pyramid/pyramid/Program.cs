c70491a86f5b22d11c73607ca4708ec79a5639a7
Ruslan <rusvas012@gmail.com>
2022-06-03 00:04:09 +0300

    add 4.2

---
+++ 4.2/pyramid/pyramid/Program.cs
@@ -0,0 +1,41 @@
+﻿using pyramid;
+
+var inp = File.ReadAllLines("input.txt");
+int height = int.Parse(inp[0]);
+
+if (height == 0)
+{
+
+    return;
+}
+
+var head = new Tree()
+{
+    Value = int.Parse(inp[1].Trim()),
+};
+
+Dictionary<(int, int), Tree> trees = new();
+trees.Add((0, 0), head);
+
+for (int i = 1; i < height; i++)
+{
+    var row = inp[i + 1].Split(" ").Select(x => int.Parse(x)).ToList();
+    for (int j = 0; j < row.Count; j++)
+    {
+        var node = new Tree()
+        {
+            Value = row[j]
+        };
+
+        if (trees.ContainsKey((i - 1, j - 1)))
+        {
+            trees[(i - 1, j - 1)].Right = node;
+        }
+        if (trees.ContainsKey((i - 1, j)))
+        {
+            trees[(i - 1, j)].Left = node;
+        }
+
+        trees.Add((i, j), node);
+    }
+}
---
+++ 4.2/pyramid/pyramid/Tree.cs
@@ -0,0 +1,8 @@
+﻿namespace pyramid;
+
+internal class Tree
+{
+    public int Value { get; set; }
+    public Tree? Left { get; set; }
+    public Tree? Right { get; set; }
+}

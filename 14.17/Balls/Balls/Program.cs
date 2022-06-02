6d8c334bb3d012204dc12f06edb1b4a257e6b628
Ruslan <rusvas012@gmail.com>
2022-06-03 00:04:26 +0300

    add 14.17

---
+++ 14.17/Balls/Balls/Program.cs
@@ -0,0 +1,25 @@
+﻿char left = '<';
+char right = '>';
+int count = 0;
+bool isOk = false;
+
+var inp = File.ReadAllLines("input.txt");
+var countBalls = int.Parse(inp[0]);
+var row = inp[1].ToCharArray();
+
+while (!isOk)
+{
+    isOk = true;
+    for (int i = 0; i < countBalls - 1; i++)
+    {
+        if (row[i] == right&& row[i + 1] == left)
+        {
+            row[i] = left;
+            row[i + 1] = right;
+            count++;
+            isOk = false;
+        }
+    }
+}
+
+File.WriteAllText("output.txt", count.ToString());

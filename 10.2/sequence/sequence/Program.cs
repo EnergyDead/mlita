172f27b53aac3c5acc0db242e6d3a950c3aa3ae1
Ruslan <rusvas012@gmail.com>
2022-06-03 00:04:39 +0300

    add 10.2

---
+++ 10.2/sequence/sequence/Program.cs
@@ -0,0 +1,37 @@
+﻿var inFile = "input.txt";
+var outFile = "output.txt";
+ulong res = 0;
+ulong count = 0;
+
+var inp = File.ReadAllLines(inFile);
+count = ulong.Parse(inp[1]);
+var row = new List<ulong>();
+row.AddRange(inp[0].Split(" ").Select(x => ulong.Parse(x)).ToList());
+
+List<int> mas = new();
+Dictionary<int, int> mem = new();
+int it;
+int base1 = -1;   // first period first element index
+int perLen = -1; // period length
+for (int i = 1; ; i++)
+{
+    mas[i] = (mas[i - 1] * mas[i - 1]) % 1111111111;
+    it = mem.ContainsKey(mas[i]);
+    if (it != mem.Last().Value)
+    {
+        base1 = it->second;
+        perLen = i - base1;
+        break;
+    }
+    else
+        mem[mas[i]] = i;
+}
+if (n <= base)
+    cout << mas[n];
+else
+{
+    int index = base + (n - base) % perLen;
+    cout << mas[index];
+}
+
+File.WriteAllText(outFile, res.ToString());
